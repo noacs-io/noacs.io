405341cbccf5ef542583148dd4f0737ed989bca8
Kelvin John Falk Szolnoky <me@kelszo.com>
2023-06-18 18:56:14 +0200

    feat: initial prototype

---
+++ tailwind.config.cjs
@@ -0,0 +1,43 @@
+module.exports = {
+	content: ['./src/**/*.{html,js,svelte}'],
+	theme: {
+		extend: {},
+		fontFamily: {
+			sans: ['Roboto', 'sans-serif']
+		}
+	},
+	plugins: [require('daisyui')],
+	daisyui: {
+		themes: [
+			'light',
+			'dark',
+			'cupcake',
+			'bumblebee',
+			'emerald',
+			'corporate',
+			'synthwave',
+			'retro',
+			'cyberpunk',
+			'valentine',
+			'halloween',
+			'garden',
+			'forest',
+			'aqua',
+			'lofi',
+			'pastel',
+			'fantasy',
+			'wireframe',
+			'black',
+			'luxury',
+			'dracula',
+			'cmyk',
+			'autumn',
+			'business',
+			'acid',
+			'lemonade',
+			'night',
+			'coffee',
+			'winter'
+		]
+	}
+};
