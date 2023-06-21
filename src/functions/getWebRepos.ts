0b459e84870e65ab6f1373ae2da1dc4903e027e1
Martin Gerdin Wärnberg <martingerdin@gmail.com>
2023-06-21 20:12:52 +0200

    Work on getWebRepos

--- src/functions/getWebRepos.ts
+++ src/functions/getWebRepos.ts
@@ -2,7 +2,12 @@ import type { OctokitResponse } from '@octokit/types/';
 
 function getWebRepos(githubData: OctokitResponse<any>) {
 	const validWebTopics = ['student-project', 'phd-project', 'research-project', 'webpublish'];
-	const webRepos = githubData.filter(repo);
+	// Filter githubData for repos that have a valid web topic
+	const webRepos = githubData.filter((repo) => {
+		repo.topics.some((topic) => validWebTopics.includes(topic));
+		return repo;
+	});
+	return webRepos;
 }
 
 export default getWebRepos;
